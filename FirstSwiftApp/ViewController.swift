a3e14f0523bf55ef10768483bf7f4c4a5fac3357
JJ <infernoj2581@gmail.com>
2017-10-06 08:53:07 -0600

    Added two buttons, and an IBOutlet and an IBAction to the UiViewController

--- FirstSwiftApp/ViewController.swift
+++ FirstSwiftApp/ViewController.swift
@@ -6,15 +6,22 @@
 //  Copyright © 2017 Dela Cruz, Juan. All rights reserved.
 //
 
-import UIKit
+import UIKit //Adds a gui component
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController  {//The colon shows the relationship between the ViewController and UIviewController
+    //ViewController obtain the information from the UiViewController, obtain their methods as well
+    @IBOutlet weak var firstButton: UIButton!
+    
+    
+    //overide changes the method from the parent class and makes it unique to the class
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad() //went into the class UIVIewController and took the super.viewDidLoad() and took that method
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func firstMethod(_ sender: UIButton) {
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
